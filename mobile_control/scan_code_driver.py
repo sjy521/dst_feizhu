964e3c7f2b56b0550e9e54a5f526a8a048402755
宋敬毅 <songjingyi@mail.shuwen.com>
2024-01-26 16:03:09 +0800

    update

--- mobile_control/scan_code_driver.py
+++ mobile_control/scan_code_driver.py
@@ -3,24 +3,39 @@ import time
 import traceback
 import sys
 import os
+
 sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..")))
 
 from dynaconf import settings
 
+from util.xpath_util import find_current_element_text
 from log_model.set_log import setup_logging
 from util.fliggy_util import FliggyModel
 from util.interface_util import select_device
+
+
+def is_ok(fliggy_model):
+    for _ in range(20):
+        xml_path = fliggy_model.adbModel.convert_to_xml()
+        if find_current_element_text(xml_path, "酒店详情"):
+            return True
+    else:
+        return False
 
 
 def run(device):
     device_id = device.get("deviceId")
     fliggy_model = FliggyModel(device_id)
     fliggy_model.open_wechat()
+    fliggy_model.click("发现")
     while True:
         try:
-            fliggy_model.click("发现")
             fliggy_model.click("扫一扫")
-            time.sleep(1)
+            if is_ok(fliggy_model):
+                fliggy_model.adbModel.click_back()
+            else:
+                print("二维码扫码错误")
+                break
         except Exception as f:
             logging.info("异常： [{}]， 准备跳过...".format(traceback.format_exc()))
             continue
