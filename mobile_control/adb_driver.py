21f23cd06d95c6623b48b82c2a62217aeb7b5600
宋敬毅 <songjingyi@mail.shuwen.com>
2024-04-30 18:29:24 +0800

    update

--- mobile_control/adb_driver.py
+++ mobile_control/adb_driver.py
@@ -38,19 +38,20 @@ def run(device):
             pay_status = fliggy_model.pay_order(pay_password)
             if pay_status:
                 pay_num += 1
-                busy_devices = select_device()
-                if len(busy_devices) > 0:
-                    for busy_device in busy_devices:
-                        if busy_device['deviceId'] == device_id:
-                            is_busy = int(busy_device.get("isBusy"))
-                if is_busy != 0:
-                    is_busy -= 1
+                # busy_devices = select_device()
+                # if len(busy_devices) > 0:
+                #     for busy_device in busy_devices:
+                #         if busy_device['deviceId'] == device_id:
+                #             is_busy = int(busy_device.get("isBusy"))
+                # if is_busy != 0:
+                #     is_busy -= 1
+                is_busy -= 1
                 set_not_effective_device(device_id, is_enable, is_busy)
                 if pay_num % 5 == 0:
                     send_abnormal_alarm_for_dingding("已经连续支付成功{}单".format(pay_num))
                 continue
             pay_num = 0
-            fliggy_model.del_order()
+            # fliggy_model.del_order()
             time.sleep(1)
         except Exception as f:
             logging.info("异常： [{}]， 准备跳过...".format(traceback.format_exc()))
