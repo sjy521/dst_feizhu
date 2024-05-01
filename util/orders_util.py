07be8fed8cfb42193c7ee50b975dc340658deca1
宋敬毅 <songjingyi@mail.shuwen.com>
2024-05-01 11:55:49 +0800

    update

--- util/orders_util.py
+++ util/orders_util.py
@@ -162,7 +162,8 @@ def build_order(device_id, tar_json):
     :param device_id:
     :return:  成功，变价，满房，失败
     """
-    url = "http://192.168.52.112:8083/fliggy/buildorder"
+    # url = "http://192.168.52.112:8083/fliggy/buildorder"
+    url = "http://build-order.bingotravel.com.cn/fliggy/buildorder"
     payload = {
         "wx_link": tar_json['wx_link'],
         "sr_name": tar_json['sr_name'],
