21995512b56a53a3aba1eb9004706828e6f3a28d
宋敬毅 <songjingyi@mail.shuwen.com>
2024-05-11 18:08:10 +0800

    update

--- order_control/snatching_orders.py
+++ order_control/snatching_orders.py
@@ -25,46 +25,44 @@ if __name__ == '__main__':
                 device_name = device_info.get('deviceName')
                 is_busy = int(device_info.get('isBusy'))
                 phone = device_info.get('accountNo')
-                for i in range(20):
-                    # 查询待处理订单并锁单
-                    res = get_effective_order(device_id, error_list, device_name)
-                    if res is not None:
-                        d_ordr_id, bg_order_id = res
-                        try:
-                            tar_json = get_url_by_bgorderid(d_ordr_id, bg_order_id)
-                            build_res = build_order(device_id, tar_json, phone)
-                            if build_res is not None:
-                                if build_res == "满房":
-                                    fail_order_unlock(0, 1, bg_order_id, device_id)
-                                    logging.info("[{}]下单完成, 满房".format(bg_order_id))
-                                elif build_res == "变价":
-                                    fail_order_unlock(1, 0, bg_order_id, device_id)
-                                    logging.info("[{}]下单完成, 变价".format(bg_order_id))
-                                else:
-                                    biz_order_id, price = build_res
-                                    is_busy += 1
-                                    set_not_effective_device(device_id, 1, is_busy)
-                                    create_order_res = order_create_order(bg_order_id, biz_order_id, price, device_id)
-                                    if create_order_res is False:
-                                        cancel_order(device_id, biz_order_id)
-                                        logging.info("[{}]补录失败, 取消订单号：[{}]".format(bg_order_id, biz_order_id))
-                                    else:
-                                        logging.info("[{}]下单完成, 订单号：[{}]".format(bg_order_id, biz_order_id))
-                                break
+                res = get_effective_order(device_id, error_list, device_name)
+                if res is not None:
+                    d_ordr_id, bg_order_id = res
+                    try:
+                        tar_json = get_url_by_bgorderid(d_ordr_id, bg_order_id)
+                        build_res = build_order(device_id, tar_json, phone)
+                        if build_res is not None:
+                            if build_res == "满房":
+                                fail_order_unlock(0, 1, bg_order_id, device_id)
+                                logging.info("[{}]下单完成, 满房".format(bg_order_id))
+                            elif build_res == "变价":
+                                fail_order_unlock(1, 0, bg_order_id, device_id)
+                                logging.info("[{}]下单完成, 变价".format(bg_order_id))
                             else:
-                                logging.info("[{}]下单失败".format(bg_order_id))
-                                error_list.append(bg_order_id)
-                                unlock(bg_order_id, device_id)
-                                time.sleep(1)
-                                continue
-                        except Exception as f:
-                            logging.error("异常：{}".format(str(traceback.format_exc())))
+                                biz_order_id, price = build_res
+                                is_busy += 1
+                                set_not_effective_device(device_id, 1, is_busy)
+                                create_order_res = order_create_order(bg_order_id, biz_order_id, price, device_id)
+                                if create_order_res is False:
+                                    cancel_order(device_id, biz_order_id)
+                                    logging.info("[{}]补录失败, 取消订单号：[{}]".format(bg_order_id, biz_order_id))
+                                else:
+                                    logging.info("[{}]下单完成, 订单号：[{}]".format(bg_order_id, biz_order_id))
+                            break
+                        else:
+                            logging.info("[{}]下单失败".format(bg_order_id))
                             error_list.append(bg_order_id)
                             unlock(bg_order_id, device_id)
                             time.sleep(1)
-                    else:
-                        logging.info("当前无待处理订单")
+                            continue
+                    except Exception as f:
+                        logging.error("异常：{}".format(str(traceback.format_exc())))
+                        error_list.append(bg_order_id)
+                        unlock(bg_order_id, device_id)
                         time.sleep(1)
+                else:
+                    logging.info("当前无待处理订单")
+                    time.sleep(1)
             else:
                 logging.info("当前无可用的设备")
                 time.sleep(10)
