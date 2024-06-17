1034ba086fb22a19a88992d314a8f96a73294ea0
宋敬毅 <songjingyi@mail.shuwen.com>
2024-06-17 16:40:26 +0800

    update

--- apply_invoice.sh
+++ apply_invoice.sh
@@ -3,3 +3,4 @@
 nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py ORHQN799EI8TUWN7 > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_ORHQN799EI8TUWN7.log 2>&1 &
 nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py DYKV7LOV8HV855JR > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_DYKV7LOV8HV855JR.log 2>&1 &
 nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py HYT4897HSSAUZLNZ > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_HYT4897HSSAUZLNZ.log 2>&1 &
+nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py 55AU4LXGOJZL7XLZ > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_55AU4LXGOJZL7XLZ.log 2>&1 &
