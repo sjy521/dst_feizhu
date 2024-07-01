312ff49da89185058ec6ae29da2794338a6cd665
宋敬毅 <songjingyi@mail.shuwen.com>
2024-07-01 23:57:01 +0800

    update

--- mt_product_price.py
+++ mt_product_price.py
@@ -27,12 +27,10 @@ async def fetch_and_save_data(session, pool, url, data, sem):
             formatted_data = []
             try:
                 for result in results['result'][0]['productRespDTOList']:
-                    print(result)
-                    formatted_data.append((result['hotelId'], result['productId'], result['totalPrice'], result['productInfo']['productLimitRule'], result['priceInfos']['date'], time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+                    formatted_data.append((result['hotelId'], result['productId'], result['totalPrice'], result['productInfo']['productLimitRule'], result['priceInfos'][0]['date'], time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
             except Exception as f:
-                print(f)
                 return None
-
+            print('通过')
             async with pool.acquire() as conn:
                 async with conn.cursor() as cursor:
                     sql = "INSERT INTO mt_product_price (hotelId, productId, totalPrice, productLimitRule, date, createTime) VALUES (%s, %s, %s, %s, %s, %s)"
