66dd2e48d39f7d3e8e88450011b4ac5744d774fd
宋敬毅 <songjingyi@mail.shuwen.com>
2024-07-01 23:27:53 +0800

    update

---
+++ mt_product_price.py
@@ -0,0 +1,94 @@
+import nest_asyncio
+import asyncio
+import aiohttp
+import pandas
+import aiomysql
+import time
+from aiohttp import ClientSession
+from asyncio import Semaphore
+
+# 允许嵌套的事件循环
+nest_asyncio.apply()
+
+total_requests = 200000
+completed_requests = 0
+lock = asyncio.Lock()
+
+# 异步函数，用于发送 POST 请求并存储结果
+async def fetch_and_save_data(session, pool, url, data, sem):
+    global completed_requests
+    async with sem:
+        async with session.post(url, json=data) as response:
+            results = await response.json()
+            async with lock:
+                completed_requests += 1
+                print(f'Progress: {completed_requests}/{total_requests} completed')
+
+            formatted_data = []
+            try:
+                for result in results['result'][0]['productRespDTOList']:
+                    formatted_data.append((result['hotelId'], result['productId'], result['totalPrice'], result['productInfo']['productLimitRule'], time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+            except Exception as f:
+                #                 print(f)
+                return None
+
+            async with pool.acquire() as conn:
+                async with conn.cursor() as cursor:
+                    sql = "INSERT INTO mt_product_price (hotelId, productId, totalPrice, productLimitRule, createTime) VALUES (%s, %s, %s, %s, %s)"
+                    await cursor.executemany(sql, formatted_data)
+                    await conn.commit()
+
+# 主函数，负责发送请求并存储结果
+async def main():
+    global total_requests
+    url = "http://10.18.99.1:8081/client/spa/batchQueryPrice"
+    ids = pandas.read_csv("./9999999999999999.csv")
+
+    sem = Semaphore(200)  # 限制并发数量为200
+
+    # 创建 MySQL 连接池
+    pool = await aiomysql.create_pool(
+        host='8.146.211.133',
+        port=3306,
+        user='bystest',
+        password='bystest',
+        db='bg_admin',
+        loop=asyncio.get_event_loop()
+    )
+
+    async with ClientSession() as session:
+        tasks = []
+        for i in ids['supplier_hotel_id']:
+            data = {
+                "checkIn": "2024-07-02",
+                "checkout": "2024-07-03",
+                "roomNum": 1,
+                "adultNum": 2,
+                "childNum": 0,
+                "childAges": [],
+                "guestType": 0,
+                "totalPrice":1000,
+
+                "suppliers": [
+                    {
+                        "supplierId": 10001,
+                        "shotelId": i
+
+                    }
+                ]
+            }
+            task = asyncio.ensure_future(fetch_and_save_data(session, pool, url, data, sem))
+            tasks.append(task)
+
+        await asyncio.gather(*tasks)
+
+    pool.close()
+    await pool.wait_closed()
+
+# 检查当前是否在运行事件循环，并选择适当的启动方式
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except RuntimeError:  # 在已有事件循环中运行
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
