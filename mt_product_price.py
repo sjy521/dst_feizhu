5e0e230161f3c281ade9fd7fed6ac76371a9f325
宋敬毅 <songjingyi@mail.shuwen.com>
2024-07-02 10:36:34 +0800

    update

--- mt_product_price.py
+++ mt_product_price.py
@@ -1,172 +1,186 @@
-import nest_asyncio
-import asyncio
-import aiohttp
+import os
+import sys
+import traceback
 import pandas
 import aiomysql
 import time
-from aiohttp import ClientSession
-from asyncio import Semaphore
+import json
+import logging
+import aiohttp
+import asyncio
+from threading import Thread
+import urllib
+from aiohttp import TCPConnector
 
-# 允许嵌套的事件循环
-nest_asyncio.apply()
+hotel_error_list = []
+passlist = []
 
-total_requests = 200000
-completed_requests = 0
-lock = asyncio.Lock()
-
-
-# 异步函数，用于发送 POST 请求并存储结果
-async def fetch_and_save_data(session, pool, url, data, sem):
-    global completed_requests
-    async with sem:
-        async with session.post(url, json=data) as response:
-            results = await response.json()
-            print(results)
-            async with lock:
-                completed_requests += 1
-                print(f'Progress: {completed_requests}/{total_requests} completed')
-
-            formatted_data = []
-            try:
-                for res in results['result']:
-                    for result in res['productRespDTOList']:
-                        if result['productId'] is None:
-                            continue
-                        formatted_data.append((result['hotelId'], result['productId'], result['totalPrice'],
-                                               result['productInfo']['productLimitRule'],
-                                               result['priceInfos'][0]['date'],
-                                               time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-            except Exception as f:
-                return None
-            print('通过')
-            async with pool.acquire() as conn:
-                async with conn.cursor() as cursor:
-                    sql = "INSERT INTO mt_product_price (hotelId, productId, totalPrice, productLimitRule, date, createTime) VALUES (%s, %s, %s, %s, %s, %s)"
-                    await cursor.executemany(sql, formatted_data)
-                    await conn.commit()
+async def fetch(session, url, hotel_data):
+    async with semaphore:
+        try:
+            for _ in range(3):
+                async with session.request("post", url, json=hotel_data) as response:
+                    res = await response.text()
+                    return res
+        except:
+            print((traceback.format_exc()))
+            return False
 
 
-# 主函数，负责发送请求并存储结果
-async def main():
-    global total_requests
+def start_thread_loop(loop):
+    """
+    启动一个loop线程
+    :param loop:
+    :return:
+    """
+
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
+
+
+async def task(hotel_data, next_cursor=None):
+    """
+    更新个人橱窗详情
+    :return:
+    """
+    #     print(name)
     url = "http://10.18.99.1:8081/client/spa/batchQueryPrice"
-    ids = pandas.read_csv("./9999999999999999.csv")
-
-    sem = Semaphore(20)  # 限制并发数量为200
-
-    # 创建 MySQL 连接池
-    pool = await aiomysql.create_pool(
-        host='8.146.211.133',
-        port=3306,
-        user='bystest',
-        password='bystest',
-        db='bg_admin',
-        loop=asyncio.get_event_loop()
-    )
-
-    async with ClientSession() as session:
-        tasks = []
-        for dat in [2, 3, 4]:
-            for i in range(len(ids['supplier_hotel_id']) - 10, -1, -10):
-                data = {
-                    "checkIn": "2024-07-0{}".format(dat),
-                    "checkout": "2024-07-0{}".format(dat + 1),
-                    "roomNum": 1,
-                    "adultNum": 2,
-                    "childNum": 0,
-                    "childAges": [],
-                    "guestType": 0,
-                    "totalPrice": 1000,
-                    "suppliers": [
-                        {
-                            "supplierId": 10001,
-                            "shotelId": str(ids['supplier_hotel_id'][i])
-
-                        },
-                        {
-                            "supplierId": 10001,
-                            "shotelId": str(ids['supplier_hotel_id'][i + 1])
-
-                        },
-                        {
-                            "supplierId": 10001,
-                            "shotelId": str(ids['supplier_hotel_id'][i + 2])
-
-                        },
-                        {
-                            "supplierId": 10001,
-                            "shotelId": str(ids['supplier_hotel_id'][i + 3])
-
-                        },
-                        {
-                            "supplierId": 10001,
-                            "shotelId": str(ids['supplier_hotel_id'][i + 4])
-
-                        }
-                    ]
-                }
-                print(data)
-                task = asyncio.ensure_future(fetch_and_save_data(session, pool, url, data, sem))
-                tasks.append(task)
-
-        await asyncio.gather(*tasks)
 
 
-    async with ClientSession() as session:
-        tasks = []
-        for dat in [2, 3, 4]:
-            for i in range(len(ids['supplier_hotel_id']) -10 , -1, -10):
-                data = {
-                    "checkIn": "2024-07-0{}".format(dat),
-                    "checkout": "2024-07-0{}".format(dat + 1),
-                    "roomNum": 1,
-                    "adultNum": 2,
-                    "childNum": 0,
-                    "childAges": [],
-                    "guestType": 0,
-                    "totalPrice": 1000,
-                    "suppliers": [
-                        {
-                            "supplierId": 10001,
-                            "shotelId": str(ids['supplier_hotel_id'][i + 5])
-
-                        },
-                        {
-                            "supplierId": 10001,
-                            "shotelId": str(ids['supplier_hotel_id'][i + 6])
-
-                        },
-                        {
-                            "supplierId": 10001,
-                            "shotelId": str(ids['supplier_hotel_id'][i + 7])
-
-                        },
-                        {
-                            "supplierId": 10001,
-                            "shotelId": str(ids['supplier_hotel_id'][i + 8])
-
-                        },
-                        {
-                            "supplierId": 10001,
-                            "shotelId": str(ids['supplier_hotel_id'][i + 9])
-
-                        }
-                    ]
-                }
-                print(data)
-                task = asyncio.ensure_future(fetch_and_save_data(session, pool, url, data, sem))
-                tasks.append(task)
-
-        await asyncio.gather(*tasks)
-
-    pool.close()
-    await pool.wait_closed()
+    conn = aiohttp.TCPConnector(ssl=True)
+    async with aiohttp.ClientSession(connector=conn) as session:
+        html = await fetch(session, url, hotel_data)
+        if html is False:
+            return None
+        await parser(html, hotel_data)
 
 
-# 检查当前是否在运行事件循环，并选择适当的启动方式
-if __name__ == "__main__":
+async def parser(html, hotel_id):
+    """
+    解析个人橱窗详情信息
+    :return:
+    """
     try:
-        asyncio.run(main())
-    except RuntimeError:  # 在已有事件循环中运行
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(main())
+        pool = await aiomysql.create_pool(
+            host='8.146.211.133',
+            port=3306,
+            user='bystest',
+            password='bystest',
+            db='bg_admin',
+            loop=asyncio.get_event_loop()
+        )
+        results = json.loads(html)
+        formatted_data = []
+        try:
+            for res in results['result']:
+                for result in res['productRespDTOList']:
+                    if result['productId'] is None:
+                        continue
+                    formatted_data.append((result['hotelId'], result['productId'], result['totalPrice'],
+                                           result['productInfo']['productLimitRule'],
+                                           result['priceInfos'][0]['date'],
+                                           time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+        except Exception as f:
+            #                 print(f)
+            return None
+
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                sql = "INSERT INTO mt_product_price (hotelId, productId, totalPrice, productLimitRule, date, createTime) VALUES (%s, %s, %s, %s, %s, %s)"
+
+                await cursor.executemany(sql, formatted_data)
+                await conn.commit()
+                print(len(formatted_data))
+    except Exception as f:
+        print(f)
+
+
+
+def run_windows_detail(res_data):
+    global semaphore, new_loop
+    new_loop = asyncio.new_event_loop()
+    semaphore = asyncio.Semaphore(20, loop=new_loop)
+    loop_thread = Thread(target=start_thread_loop, args=(new_loop,))
+    # loop_thread.setDaemon(True)
+    loop_thread.start()
+    ids = pandas.read_csv("/Users/xhzy/Desktop/9999999999999999.csv")
+    for dat in [2, 3, 4]:
+        tasks = []
+        for i in range(len(ids['supplier_hotel_id']) -10, -1, -10):
+
+            data = {
+                "checkIn": "2024-07-0{}".format(dat),
+                "checkout": "2024-07-0{}".format(dat + 1),
+                "roomNum": 1,
+                "adultNum": 2,
+                "childNum": 0,
+                "childAges": [],
+                "guestType": 0,
+                "totalPrice":1000,
+
+                "suppliers": [
+                    {
+                        "supplierId": 10001,
+                        "shotelId": str(ids['supplier_hotel_id'][i])
+
+                    },
+                    {
+                        "supplierId": 10001,
+                        "shotelId": str(ids['supplier_hotel_id'][i+1])
+
+                    },
+                    {
+                        "supplierId": 10001,
+                        "shotelId": str(ids['supplier_hotel_id'][i+2])
+
+                    },
+                    {
+                        "supplierId": 10001,
+                        "shotelId": str(ids['supplier_hotel_id'][i+3])
+
+                    },
+                    {
+                        "supplierId": 10001,
+                        "shotelId": str(ids['supplier_hotel_id'][i+4])
+
+                    },
+                    {
+                        "supplierId": 10001,
+                        "shotelId": str(ids['supplier_hotel_id'][i+5])
+
+                    },
+                    {
+                        "supplierId": 10001,
+                        "shotelId": str(ids['supplier_hotel_id'][i+6])
+
+                    },
+                    {
+                        "supplierId": 10001,
+                        "shotelId": str(ids['supplier_hotel_id'][i+7])
+
+                    },
+                    {
+                        "supplierId": 10001,
+                        "shotelId": str(ids['supplier_hotel_id'][i+8])
+
+                    },
+                    {
+                        "supplierId": 10001,
+                        "shotelId": str(ids['supplier_hotel_id'][i+9])
+
+                    }
+                ]
+            }
+
+            asyncio.run_coroutine_threadsafe(task(data), new_loop)
+
+
+if __name__ == '__main__':
+    # source_id = '102540320683'
+    # sec_uid = 'MS4wLjABAAAAS7KFopXZsSEE6EuEtKS5PcyJrBSTOldSL3SUeTZGfqM'
+    #     data = pandas.read_csv('/Users/xhzy/Desktop/直播榜.csv')
+
+    run_windows_detail('')
+#     pandas.DataFrame(douyindatalist).to_csv('/Users/xhzy/Desktop/new_test1.csv')
+
+
