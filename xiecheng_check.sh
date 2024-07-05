218bd00ea2783115e4ccad2b4b226e002a6af6fe
宋敬毅 <songjingyi@mail.shuwen.com>
2024-07-05 11:12:54 +0800

    update

---
+++ xiecheng_check.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/xiecheng_check.py > /root/bgProjects/fliggy-mobile-control/logs/xiecheng_check.log 2>&1 &
