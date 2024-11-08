f10fceca0acf7425f7b2b4f12eb04b40e799a2b1
宋敬毅 <songjingyi@mail.shuwen.com>
2024-11-08 12:15:28 +0800

    update

--- util/fliggy_util.py
+++ util/fliggy_util.py
@@ -277,7 +277,7 @@ class FliggyModel:
         """
         for _ in range(10):
             for __ in range(3):
-                time.sleep(4)
+                time.sleep(6)
                 xml_path = self.adbModel.convert_to_xml(self.device_id)
                 time.sleep(1)
                 if find_current_element_text(xml_path, "全部订单"):
