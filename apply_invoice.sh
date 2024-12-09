24fc07578229f96b203296945838402a4073886e
宋敬毅 <songjingyi@mail.shuwen.com>
2024-12-09 18:37:44 +0800

    update

--- apply_invoice.sh
+++ apply_invoice.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py ORHQN799EI8TUWN7 > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_ORHQN799EI8TUWN7.log 2>&1 &
-nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py DYKV7LOV8HV855JR > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_DYKV7LOV8HV855JR.log 2>&1 &
-nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py HYT4897HSSAUZLNZ > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_HYT4897HSSAUZLNZ.log 2>&1 &
-nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py 55AU4LXGOJZL7XLZ > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_55AU4LXGOJZL7XLZ.log 2>&1 &
-nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py 4TUGJN79S89HKBOF > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_4TUGJN79S89HKBOF.log 2>&1 &
-nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py EMWGCACYINKVR8KB > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_EMWGCACYINKVR8KB.log 2>&1 &
-nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py MRGMCU4TZXKJXGLZ > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_MRGMCU4TZXKJXGLZ.log 2>&1 &
+#nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py ORHQN799EI8TUWN7 > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_ORHQN799EI8TUWN7.log 2>&1 &
+#nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py DYKV7LOV8HV855JR > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_DYKV7LOV8HV855JR.log 2>&1 &
+#nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py HYT4897HSSAUZLNZ > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_HYT4897HSSAUZLNZ.log 2>&1 &
+#nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py 55AU4LXGOJZL7XLZ > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_55AU4LXGOJZL7XLZ.log 2>&1 &
+#nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py 4TUGJN79S89HKBOF > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_4TUGJN79S89HKBOF.log 2>&1 &
+#nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py EMWGCACYINKVR8KB > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_EMWGCACYINKVR8KB.log 2>&1 &
+#nohup /root/bgProjects/fliggy-mobile-control/venv/bin/python3 /root/bgProjects/fliggy-mobile-control/invoice_control/apply_invoice.py MRGMCU4TZXKJXGLZ > /root/bgProjects/fliggy-mobile-control/logs/apply_invoice_MRGMCU4TZXKJXGLZ.log 2>&1 &
+
+nohup python3 /home/fliggy-mobile-control/fliggy-mobile-control/invoice_control/apply_invoice.py ORHQN799EI8TUWN7 > /home/fliggy-mobile-control/fliggy-mobile-control/logs/apply_invoice_ORHQN799EI8TUWN7.log 2>&1 &
+nohup python3 /home/fliggy-mobile-control/fliggy-mobile-control/invoice_control/apply_invoice.py DYKV7LOV8HV855JR > /home/fliggy-mobile-control/fliggy-mobile-control/logs/apply_invoice_DYKV7LOV8HV855JR.log 2>&1 &
+nohup python3 /home/fliggy-mobile-control/fliggy-mobile-control/invoice_control/apply_invoice.py HYT4897HSSAUZLNZ > /home/fliggy-mobile-control/fliggy-mobile-control/logs/apply_invoice_HYT4897HSSAUZLNZ.log 2>&1 &
+nohup python3 /home/fliggy-mobile-control/fliggy-mobile-control/invoice_control/apply_invoice.py 55AU4LXGOJZL7XLZ > /home/fliggy-mobile-control/fliggy-mobile-control/logs/apply_invoice_55AU4LXGOJZL7XLZ.log 2>&1 &
+nohup python3 /home/fliggy-mobile-control/fliggy-mobile-control/invoice_control/apply_invoice.py 4TUGJN79S89HKBOF > /home/fliggy-mobile-control/fliggy-mobile-control/logs/apply_invoice_4TUGJN79S89HKBOF.log 2>&1 &
+nohup python3 /home/fliggy-mobile-control/fliggy-mobile-control/invoice_control/apply_invoice.py EMWGCACYINKVR8KB > /home/fliggy-mobile-control/fliggy-mobile-control/logs/apply_invoice_EMWGCACYINKVR8KB.log 2>&1 &
+nohup python3 /home/fliggy-mobile-control/fliggy-mobile-control/invoice_control/apply_invoice.py MRGMCU4TZXKJXGLZ > /home/fliggy-mobile-control/fliggy-mobile-control/logs/apply_invoice_MRGMCU4TZXKJXGLZ.log 2>&1 &
+
