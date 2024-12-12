64e51aa21f98278525acd5cecdf393b620cf408b
宋敬毅 <songjingyi@mail.shuwen.com>
2024-12-12 17:23:18 +0800

    update

--- pjy/pjy.py
+++ pjy/pjy.py
@@ -179,7 +179,7 @@ def use_thread_pool():
             successlist = []
             if is_five_pm():
                 send_dingding("9 秒后准备预约！！！")
-                time.sleep(8.7)
+                time.sleep(8.4)
                 for j in range(20):
                     # 提交任务到线程池中
                     future_to_result = {executor.submit(send_request, i): i for i in openlist}
