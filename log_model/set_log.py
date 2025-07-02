066752332adfad1c497bb2fd8fec8f55cb396e5a
宋敬毅 <songjingyi@mail.shuwen.com>
2025-07-02 17:59:46 +0800

    update

--- log_model/set_log.py
+++ log_model/set_log.py
@@ -8,9 +8,11 @@ from dynaconf import settings
 def setup_logging(default_path=settings.LOGGING, default_level=logging.DEBUG):
     path = os.path.abspath(os.path.join(os.getcwd(), "..")) + default_path
     if os.path.exists(path):
+        logging.info("加载了日志文件")
         with open(path, "r") as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
             log_config.dictConfig(config)
     else:
+        logging.info("没有加载日志文件")
         logging.basicConfig(level=default_level)
 
